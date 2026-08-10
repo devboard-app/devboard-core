3cefb23fffff3cc9a9ad4deda58ff223259f4478
Constantin Ilie <intern.constantin.ilie@assist.ro>
2026-08-10 14:40:17 +0300

    feat: add user sync endpoint (POST /api/users/sync/)

---
+++ users/views.py
@@ -0,0 +1,16 @@
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from .serializers import UserProfileSerializer
+
+# Create your views here.
+
+class SyncUserView(APIView):
+    def post(self, request):
+        serializer = UserProfileSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
